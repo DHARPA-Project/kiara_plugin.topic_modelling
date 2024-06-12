2c5eb45d15378bfe73657dbe1f7092efb0746bfb
Mariella CC <MariellaCC@users.noreply.github.com>
2024-06-12 12:56:15 +0200

    wip onboarding

--- docs/streamlit/streamlit.py
+++ docs/streamlit/streamlit.py
@@ -4,10 +4,17 @@ from kiara.api import KiaraAPI
 
 kiara = KiaraAPI.instance()
 
-st.header("1. Onboarding")
+#with st.sidebar:
+    # mettre un formulaire pour le data lineage
+    # mettre un formulaire pour explication operation
 
-tab_ob_local, tab_ob_github, tab_ob_zenodo = st.tabs(["Local folder", "Github", "Zenodo archive"])
+st.header("Data Onboarding")
 
+tab_ob_local, tab_ob_github, tab_ob_zenodo, tab_ob_store = st.tabs(["Local folder", "Github", "Zenodo archive", "Data store"])
+# add data store as onboarding source
+
+
+# tab get data from local folder
 with tab_ob_local:
     st.subheader("Get files from local folder")
   
@@ -16,7 +23,7 @@ with tab_ob_local:
     #### user inputs ####
     local_folder_path = ob_local_files_form.text_input(
         "Local folder path:",
-        "/path/to/folder",
+        "/Users/mariella.decrouychan/Documents/GitHub/kiara_plugin.topic_modelling/tests/resources/data/text_corpus/data",
         key = "local-folder-path",
     )
 
@@ -36,24 +43,30 @@ with tab_ob_local:
         "path": local_folder_path,
         }
 
-
-
         try:
             ob_local_file_bundle_results = kiara.run_job('import.table.from.local_folder_path', inputs=ob_local_file_bundle_gh_inputs, comment=ob_local_files_comment)
             ob_local_file_bundle_results_data = ob_local_file_bundle_results['table'].data
         
             #### Display results ####
             ob_local_files_res = st.form(key="ob_local_files_res")
-            ob_local_files_col1, ob_local_files_col2 = st.columns(2)
 
-            """
-            with ob_local_files_col1:
-                gh_files_results_confirmed = gh_files_res.form_submit_button("Confirm")
-                
-            with ob_local_files_col2:
-                st.write(f"Number of files: {dl_file_bundle_gh_results_data.number_of_files}")
-                st.json(dl_file_bundle_gh_results_data.included_files) 
-            """       
+            ob_local_files_res.write("Check the result in display preview below and name the table before confirming to add it to the data store.")
+
+            ob_local_files_name = ob_local_files_res.text_input(
+                    "Name:",
+                    "my_data_item_name",
+                    key="ob-save-table-name",
+                )
+            
+            ob_local_files_res_submitted = ob_local_files_res.form_submit_button("Submit")
+
+            if ob_local_files_res_submitted == True:
+                kiara.store_value(ob_local_file_bundle_results['table'].data, ob_local_files_name)
+                st.write("Data item added to the data store.")
+
+            with ob_local_files_res.expander("Display preview"):
+                st.dataframe(ob_local_file_bundle_results_data.to_pandas_dataframe())
+ 
 
         except Exception as e:
             st.error(f"An error occurred: {e}")
